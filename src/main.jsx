5f8cd483c554c399dbaecf8f48c194d473143f03
SUBHAN941 <subhan.mughal89000@gmail.com>
2025-11-07 15:42:24 +0500

    seo

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
-import { StrictMode } from 'react'
 import React from 'react'
-import { createRoot } from 'react-dom/client'
+import ReactDOM from 'react-dom/client'
+import { HelmetProvider } from 'react-helmet-async'
+import App from './App'
 import './index.css'
-import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <HelmetProvider>
     <App />
-  </StrictMode>,
+  </HelmetProvider>
 )
